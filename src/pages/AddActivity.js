3a9c7ca68176129827e526a65308fb0e8b3849a4
John <j_le_75@hotmail.com>
2020-07-31 17:56:23 -0700

    activity-add: added input types, labels, and add button

--- src/pages/AddActivity.js
+++ src/pages/AddActivity.js
@@ -1,6 +1,6 @@
 // 3rd party imports
 import React from "react";
-import { Box, TextField } from "@material-ui/core";
+import { Box, TextField, Button } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
 // My imports
@@ -8,12 +8,35 @@ import SearchIcon from "@material-ui/icons/Search";
 const AddActivityPage = () => {
   return (
     <React.Fragment>
-      <Box display="flex">
-        <TextField label="Name of Activity" />
-        <TextField label="Type of Activity" />
-        <TextField label="Location" />
-        <TextField label="Start Time" />
-        <TextField label="End Time" />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        m={4}
+      >
+        <Box m={2} display="flex" flexDirection="column">
+          <label htmlFor="activity-name">Name of Activity</label>
+          <TextField type="text" id="activity-name" />
+        </Box>
+        <Box m={2} display="flex" flexDirection="column">
+          <label htmlFor="activity-type">Type of Activity</label>
+          <TextField type="text" id="activity-type" />
+        </Box>
+        <Box m={2} display="flex" flexDirection="column">
+          <label htmlFor="activity-address">Address</label>
+          <TextField type="text" id="activity-address" />
+        </Box>
+        <Box m={2} display="flex" flexDirection="column">
+          <label htmlFor="activity-start-time">Start Time</label>
+          <TextField type="datetime-local" id="activity-start-time" />
+        </Box>
+        <Box m={2} display="flex" flexDirection="column">
+          <label htmlFor="activity-end-time">End Time</label>
+          <TextField type="datetime-local" id="activity-end-time" />
+        </Box>
+        <Button>Add</Button>
       </Box>
     </React.Fragment>
   );
